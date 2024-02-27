5961938b00a0667cea5a83fb3d85489b046f97ba
Anar-Mammadzada <anarmammadza@gmail.com>
2024-02-27 17:35:54 +0400

    Some code refactor for 'nested component and props'

---
+++ blog-posts/src/UserCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const UserCard = (props) =>{
+    return(
+        <div className='ui Card'>
+            <div className='content'>
+                <div className='header'>
+                    Anar Mammadzada
+                </div>
+                <div className='description'>
+                    {props.children}
+                </div>
+            </div>
+            <div className='ui bottom button'>
+                    <i className='add icon'></i>
+                    Add Friend
+            </div>
+        </div>
+    )
+}
+
+
+export default UserCard;
--- blog-posts/src/index.js
+++ blog-posts/src/index.js
@@ -1,63 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SingleComment from '../src/SingleComment';
 import profile1 from './image/Arunas.jpg';
 import profile2 from './image/ayaz.jpg';
-import profile3 from './image/Manis.jpg'
+import profile3 from './image/Manis.jpg';
+import UserCard from './UserCard';
 
 const App = () => {
     return (
-        <div className="ui comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={profile1} alt=''/>
-                </a>
-                <div className="content">
-                <a href="/" className="author">Anar</a>
-                <div className="metadata">
-                    <span className="date">
-                        Today at 5:00 PM
-                    </span>
+        <div className='ui comments'>
+            <UserCard>
+                <div>
+                    Hello, my name is Anar. I live in Baku.
                 </div>
-                <div className="text">
-                    it's amazing
-                </div>
-                </div>
-            </div>
-
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={profile2} alt=''/>
-                </a>
-                <div className="content">
-                <a href="/" className="author">Ayaz</a>
-                <div className="metadata">
-                    <span className="date">
-                        Today at 5:00 PM
-                    </span>
-                </div>
-                <div className="text">
-                    it's amazing
-                </div>
-                </div>
-            </div>
-
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={profile3} alt=''/>
-                </a>
-                <div className="content">
-                <a href="/" className="author">Manis</a>
-                <div className="metadata">
-                    <span className="date">
-                        Today at 5:00 PM
-                    </span>
-                </div>
-                <div className="text">
-                    it's amazing
-                </div>
-                </div>
-            </div>
-
+            </UserCard>
+            <UserCard>
+                <SingleComment name='Arunas' date='Today at 5:00 PM' text='its amazing' picture={profile1} />
+            </UserCard>
+            <UserCard>
+                <SingleComment name='Ayaz' date='Today at 6:00 AM' text='I am qalosh' picture={profile2} />
+            </UserCard>
+            <UserCard>
+                <SingleComment name='Manis' date='Yesterday at 11:00 PM' text='I am manis kopoyoglu' picture={profile3} />
+            </UserCard>
         </div>
     )
 }
